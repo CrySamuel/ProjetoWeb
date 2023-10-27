21214d541d9026adefb64157499931b80a8613f0
CrySamuel <crystoferdemetino@gmail.com>
2023-10-26 21:58:51 -0300

    Informações sobre produto puxado pelo Banco de dados esta pegando

--- BackEnd/getProduct.php
+++ BackEnd/getProduct.php
@@ -1,4 +1,14 @@
 <?php
 
+    $con = mysqli_connect('localhost:3306', 'root', '2810leticia', 'lojaweb');
 
+    $resultado = mysqli_query($con,'SELECT * FROM produtos');
+
+    $dados = array();
+    while($registro = mysqli_fetch_assoc($resultado)){
+        array_push($dados, $registro);
+    }
+
+    $json = json_encode($dados);
+    echo $json;
 ?>
