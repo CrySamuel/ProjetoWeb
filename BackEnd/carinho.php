ff385c14526f9be93de4400e70a8adcaebfefea1
CrySamuel <crystoferdemetino@gmail.com>
2023-10-29 16:57:53 -0300

    Pagina de Adicionar do ADM feita por completo

---
+++ BackEnd/carinho.php
@@ -0,0 +1,16 @@
+<?php
+
+    $con = mysqli_connect('localhost:3306', 'root', '2810leticia', 'lojaweb');
+
+    $resultado = mysqli_query($con, "SELECT * FROM  carrinho");
+
+    $dados = array();
+
+    while($registro = mysqli_fetch_assoc($resultado)){
+
+        array_push($dados, $registro);
+    }
+
+    $json = json_encode($dados);
+    echo $json;
+?>
